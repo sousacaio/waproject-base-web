a30e8fc3346283e3ae126769aaed103e2059922a
Shouganaii <frias.caio@yahoo.com.br>
2021-05-22 23:58:01 -0300

    feat: creation of  order dialog

--- src/components/Pages/Admin/Orders/FormDialog.tsx
+++ src/components/Pages/Admin/Orders/FormDialog.tsx
@@ -4,15 +4,12 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
 import FormHelperText from '@material-ui/core/FormHelperText';
-import FormLabel from '@material-ui/core/FormLabel';
 import Grid from '@material-ui/core/Grid';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Slide from '@material-ui/core/Slide';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import ErrorMessage from 'components/Shared/ErrorMessage';
-import CheckboxField from 'components/Shared/Fields/Checkbox';
 import TextField from 'components/Shared/Fields/Text';
 import Toast from 'components/Shared/Toast';
 import { logError } from 'helpers/rxjs-operators/logError';
@@ -32,10 +29,10 @@ interface IProps {
 }
 
 const validationSchema = yup.object().shape({
-  firstName: yup.string().required().min(3).max(50),
-  lastName: yup.string().required().min(3).max(50),
-  email: yup.string().required().email().max(150),
-  roles: yup.array().required().min(1)
+  name: yup.string().required().min(3).max(50),
+  description: yup.string().required().min(5),
+  userId: yup.number().required(),
+  quantity: yup.number().required().min(1)
 });
 
 const useStyle = makeStyles({
@@ -98,33 +95,19 @@ const FormDialog = memo((props: IProps) => {
             <Fragment>
               <Grid container spacing={2}>
                 <Grid item xs={12} sm={6}>
-                  <TextField label='Nome' name='firstName' formik={formik} />
+                  <TextField label='Nome' name='name' formik={formik} />
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                  <TextField label='Sobrenome' name='lastName' formik={formik} />
+                  <TextField label='Quantidade' name='quantity' formik={formik} />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField label='Descrição' name='description' formik={formik} />
                 </Grid>
               </Grid>
-
-              <TextField label='Email' name='email' type='email' formik={formik} />
-
               <FormControl component='fieldset' error={formik.touched.roles && !!formik.errors.roles}>
-                <FormLabel component='legend'>Acesso</FormLabel>
                 {formik.touched.roles && !!formik.errors.roles && (
                   <FormHelperText>{formik.errors.roles}</FormHelperText>
                 )}
-                <FormGroup>
-                  {roles?.map(role => (
-                    <CheckboxField
-                      key={role.role}
-                      name='roles'
-                      label={role.name}
-                      description={role.description}
-                      value={role.role}
-                      isMultiple
-                      formik={formik}
-                    />
-                  ))}
-                </FormGroup>
               </FormControl>
             </Fragment>
           )}
